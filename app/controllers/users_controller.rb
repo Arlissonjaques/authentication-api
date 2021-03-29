107377736286f15438c06c09e15b2e985e2830be
Arlisson Jaques <arlissonjaques@gmail.com>
2021-03-29 00:58:21 -0300

    feat(user_controller): added show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: %i[show]
+
   def index
     render json: User.all
+  end
+
+  def show
+    render json: @users
+  end
+
+  private
+
+  def set_user
+    @users = User.find(params[:id])
   end
 end
