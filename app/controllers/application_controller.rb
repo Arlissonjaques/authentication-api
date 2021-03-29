37652e152369bab0aee45b9f4869bc5b9c078c95
Arlisson Jaques <arlissonjaques@gmail.com>
2021-03-29 13:30:04 -0300

    feat(policy): add policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  include Pundit
   include DeviseTokenAuth::Concerns::SetUserByToken
 
   before_action :configure_permitted_parameters, if: :devise_controller?
