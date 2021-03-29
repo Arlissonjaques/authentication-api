8f21329a51ce37036af6163a8f959fd6025e2932
Arlisson Jaques <arlissonjaques@gmail.com>
2021-03-29 01:12:39 -0300

    feat(application_controller): allowing user parameters to be updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,6 @@ class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :role])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email])
   end
 end
