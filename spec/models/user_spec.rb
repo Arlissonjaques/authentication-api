c8d9d89c5ab477b03715d08d9916aee4827e7998
Arlisson Jaques <arlissonjaques@gmail.com>
2021-03-30 12:35:21 -0300

    test(model_user): validation test added

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:role) }
+  end
+end
