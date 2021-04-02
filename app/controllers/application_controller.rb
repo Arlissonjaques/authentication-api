f5d168bdebe5e48e2378b85c11957e82c6f92b26
Arlisson Jaques <arlissonjaques@gmail.com>
2021-04-01 23:31:49 -0300

    refactor(application_controller): making the method private

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :role])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email])
