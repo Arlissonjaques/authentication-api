c610cdb36e64511cbbb18cc2c39985b7da65a8c8
Arlisson Jaques <arlissonjaques@gmail.com>
2021-04-02 22:57:05 -0300

    refactor(migration): reactivating allow_password_change

--- db/migrate/20210328180648_devise_token_auth_create_users.rb
+++ db/migrate/20210328180648_devise_token_auth_create_users.rb
@@ -12,7 +12,7 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.1]
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
-      # t.boolean  :allow_password_change, :default => false
+      t.boolean  :allow_password_change, :default => false
 
       # ## Rememberable
       # t.datetime :remember_created_at
